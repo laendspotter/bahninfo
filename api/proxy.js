b798ffd2466b0febc606670bf5b53291b490bc3f
laendspotter <laendspotter@gmail.com>
2026-04-02 19:04:55 +0200

    Add files via upload

--- api/proxy.js
+++ api/proxy.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   const { path, ...rest } = req.query;
   if (!path) return res.status(400).json({ error: 'no path' });
   const params = new URLSearchParams(rest).toString();
